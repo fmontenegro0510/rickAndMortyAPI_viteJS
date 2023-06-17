9ce9d8b02c28ca7167cf6e1350f2751efe91613d
Francisco Montenegro <franciscomontenegro22@gmail.com>
2023-06-16 21:21:40 -0300

    added logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
 import { useState, useEffect } from 'react'
 import Header from './components/Header/Header'
 import Search from './components/Search/Search'
-import CardList from './components/CardsList/CardList'
+import CardList from './components/CardsList/CardList.jsx'
 import Footer from './components/Footer/Footer'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://rickandmortyapi.com/api/character');
-      const jsonData = await response.json();
-      setData(jsonData.results);
-      console.log(jsonData.results)
+      try {
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        const data = await response.json();
+        setCharacters(data.results);
+      } catch (error) {
+        console.error(error);
+      }
     };
-    fetchData()
+
+    fetchData();
   }, []);
+
+  const filteredCharacters = characters.filter(character =>
+    character.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <>   
-    <Header />
-    <Search />
-    <CardList data={data} />
-    <Footer />
+    <>
+<Header />
+<Search />
+<CardList />
+<Footer />
     </>
   )
 }
